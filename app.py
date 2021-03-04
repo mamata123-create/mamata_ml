6f93b18ec7a87e8691092a03e829c1043ef5ae2c
Mamata M L <mamatalokeshwar23@gmail.com>
2021-03-04 10:46:04 +0530

    app.py modified

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'welcome to our newsletter'
+    return 'welcome to our newsletter..-)'
 
 
 if __name__ == '__main__':
     app.run()
+
+print("hiiiii")
